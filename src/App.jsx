3c6a6a43a87f9e714dd406f7d7a7eca01ea9ab37
andryraz <tiavinarazafindratsimba20@gmail.com>
2024-10-03 09:34:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Liste from './components/Liste';
+import Detail from './components/Detail';
+import './styles.css';
+
+
+const App = () => {
+   
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Liste />} />
+                <Route path="/Detail" element={<Detail /> } />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
---
+++ src/components/Liste.jsx
@@ -0,0 +1,57 @@
+const apartments =  [
+        {
+          id: 1,
+          description: "A cozy 2-bedroom apartment located downtown with a fully equipped kitchen and a balcony.",
+          price: 1200,
+          picture: "https://picsum.photos/200"
+        },
+        {
+          id: 2,
+          description: "Spacious 3-bedroom apartment in a quiet neighborhood, close to public transport and schools.",
+          price: 1500,
+          picture: "https://picsum.photos/200"
+        },
+        {
+          id: 3,
+          description: "Modern 1-bedroom loft with large windows and a beautiful city view, located near restaurants and shops.",
+          price: 1000,
+          picture: "https://picsum.photos/200"
+        },
+        {
+          id: 4,
+          description: "Luxury 4-bedroom penthouse with a private rooftop terrace and high-end appliances.",
+          price: 2500,
+          picture: "https://picsum.photos/200"
+        },
+        {
+          id: 5,
+          description: "Affordable studio apartment perfect for students, within walking distance of the university.",
+          price: 800,
+          picture: "https://picsum.photos/200"
+        },
+        {
+          id: 6,
+          description: "Charming 2-bedroom apartment in a historic building, featuring wooden floors and original architecture.",
+          price: 1300,
+          picture: "https://picsum.photos/200"
+        }
+      ]      
+
+
+const Liste = () => {
+    return (
+        <div className="home-container">
+            <h2>Liste des appartements disponibles</h2>
+            <div className="apart-list">
+                {apartments.map(apart => (
+                    <div key={apart.id} className="apart-item">
+                        <img src={apart.picture} alt={apart.price} />
+                        <h3>{apart.price}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Liste;
